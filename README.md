bf459c9d87754b3fc68d138132c187111e7ca004
motoroboto <robert.a.cullen@gmail.com>
2020-11-02 21:22:40 -0500

    creating more structure

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Cool Stuff
-            This was made for Robert and it's really something
+
+# what
+[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)
+This was made for robert and it's really cool
+wen 
+how 
+where and then some
+yes
+why
+
--- index.js
+++ index.js
@@ -19,12 +19,59 @@ inquirer
             type: 'input',
             message: 'What is the name of this project?',
         },
+        {
+            name: 'useCase',
+            type: 'input',
+            message: 'What is this application used for?',
+        },
+        {
+            name: 'howTo',
+            type: 'input',
+            message: 'How is this app used?',
+        },
+        {
+            name: 'install',
+            type: 'input',
+            message: 'What command should be run to install this app?',
+        },
+        {
+            name: 'report',
+            type: 'input',
+            message: 'What should a user do to report an issue?',
+        },
+        {
+            name: 'contribute',
+            type: 'input',
+            message: 'How would someone contribute to this project?',
+        },
+        {
+            name: 'licence',
+            type: 'list',
+            message: 'How kind of license does this project have?',
+            choices: ["Apache", "MPS", "Another made Up one"],
+        },
     ])
     .then((answer) => {
+        var licenseType = answer.licence;
+        if (licenseType = 'Apache') {
+            var licenseCode = '[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)'
+        } else {
+
+        };
+        console.log('licenseType:', licenseType);
         fs.writeFile('README.md',
 
-            `# ${answer.title}
-            This was made for ${answer.name} and it's really ${answer.something}`,
+            `
+# ${answer.title}
+${licenseCode}
+This was made for ${answer.name} and it's really ${answer.something}
+${answer.useCase}
+${answer.howTo}
+${answer.install} and then some
+${answer.report}
+${answer.contribute}
+
+`,
 
             function (err) {
                 if (err) throw err;
