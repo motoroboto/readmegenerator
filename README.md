38e166072290ecbbbbae79820650431b7f5706b8
motoroboto <robert.a.cullen@gmail.com>
2020-11-03 22:52:12 -0500

    expanding the readme generation

---
+++ README.md
@@ -0,0 +1,27 @@
+
+# Godzilla Man
+[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)
+
+### Introduction
+
+This was made for Robert and it's really cool. My github can be found at https://github.com/motoroboto.
+
+[Project Description](#project-description)  
+[Installation](#installation)  
+[Issues](#issues-and-questions)  
+[License](#license)  
+
+
+### Project Description   
+This is an app that was designed to do things.   
+You can track down things and then do them.   
+
+### Installation  
+npm i and then some  
+
+### Issues and Questions
+If you find any issues with our code, please don't hesitate to contact us at robert.a.cullen@gmail.com, and if you would like to contribute to this project, we could use someone who is able to front end and data entry. If you are interested in pitching in get in touch with us. 
+
+### License  
+About the GNU v3.0 License:   
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.If not, see https://www.gnu.org/licenses/.
--- index.js
+++ index.js
@@ -37,7 +37,7 @@ inquirer
         {
             name: 'contribute',
             type: 'input',
-            message: 'How would someone contribute to this project?',
+            message: 'What kinds of contributions do you need for this project?',
         },
         {
             name: 'github',
@@ -58,7 +58,7 @@ inquirer
             var licenseDescription = 'Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0';
         } else if (answer.license == 'GNU v3.0') {
             var licenseBadge = '[![License: GPL v3](https://img.shields.io/badge/License-GPL%20v3-blue.svg)](http://www.gnu.org/licenses/gpl-3.0)';
-            var licenseDescription = 'The GNU General Public License is a free, copyleft license for software and other kinds of works. The licenses for most software and other practical works are designed to take away your freedom to share and change the works.By contrast, the GNU General Public License is intended to guarantee your freedom to share and change all versions of a program--to make sure it remains free software for all its users.We, the Free Software Foundation, use the GNU General Public License for most of our software; it applies also to any other work released this way by its authors.You can apply it to your programs, too.';
+            var licenseDescription = 'This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.If not, see https://www.gnu.org/licenses/.';
         } else if (answer.license == 'MIT') {
             var licenseBadge = '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
             var licenseDescription = 'Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.';
@@ -71,27 +71,26 @@ ${licenseBadge}
 
 ### Introduction
 
-This was made for ${answer.name} and it's really cool. My github name is ${answer.github}
+This was made for ${answer.name} and it's really cool. My github can be found at https://github.com/${answer.github}.
 
-[Project Description](#project-description)
-[Installation](#installation)
-[Issues](#issues)
-[License](#license)
+[Project Description](#project-description)  
+[Installation](#installation)  
+[Issues](#issues-and-questions)  
+[License](#license)  
 
 
-### Project Description
-${answer.useCase}
-${answer.howTo}
+### Project Description  
+${answer.useCase}  
+${answer.howTo}  
 
-### Installation
-${answer.install} and then some
+### Installation  
+${answer.install} and then some  
 
-### Issues
-${answer.report}
-${answer.contribute}
+### Issues and Questions
+If you find any issues with our code, please don't hesitate to contact us at ${answer.report}, and if you would like to contribute to this project, we could use someone who is able to ${answer.contribute}. If you are interested in pitching in get in touch with us. 
 
-### Licence
-About the ${answer.license} License: 
+### License  
+About the ${answer.license} License:   
 ${licenseDescription}
 `,
 
